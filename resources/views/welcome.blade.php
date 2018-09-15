17d6f0f6f17ae960c6e3c604453eb8d258572bd8
soumyaMondal72 <soumya.mondal72@gmail.com>
2018-09-16 02:18:37 +0530

    Update Code

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -78,7 +78,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel App
                 </div>
 
                 <div class="links">
